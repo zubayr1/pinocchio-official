9741bbf5ded8b91c8c79a7dfd122df3b7219311d
Jose Santos <60839474+Jars1987@users.noreply.github.com>
2025-05-06 14:50:20 +0100

    added from_acccount_info (#151)

--- sdk/pinocchio/src/sysvars/clock.rs
+++ sdk/pinocchio/src/sysvars/clock.rs
@@ -1,7 +1,18 @@
 //! Information about the network's clock, ticks, slots, etc.
 
 use super::Sysvar;
-use crate::impl_sysvar_get;
+use crate::{
+    account_info::{AccountInfo, Ref},
+    impl_sysvar_get,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+/// The ID of the clock sysvar.
+pub const CLOCK_ID: Pubkey = [
+    6, 167, 213, 23, 24, 199, 116, 201, 40, 86, 99, 152, 105, 29, 94, 182, 139, 94, 184, 163, 155,
+    75, 109, 92, 115, 85, 91, 33, 0, 0, 0, 0,
+];
 
 /// The unit of time given to a leader for encoding a block.
 ///
@@ -64,4 +75,68 @@ pub const DEFAULT_MS_PER_SLOT: u64 = 1_000 * DEFAULT_TICKS_PER_SLOT / DEFAULT_TI
 
 impl Sysvar for Clock {
     impl_sysvar_get!(sol_get_clock_sysvar);
+}
+
+impl Clock {
+    /// The length of the `Clock` sysvar account data.
+    pub const LEN: usize = 8 + 8 + 8 + 8 + 8;
+
+    /// Return a `Clock` from the given account info.
+    ///
+    /// This method performs a check on the account info key.
+    #[inline]
+    pub fn from_account_info(account_info: &AccountInfo) -> Result<Ref<Clock>, ProgramError> {
+        if account_info.key() != &CLOCK_ID {
+            return Err(ProgramError::InvalidArgument);
+        }
+        Ok(Ref::map(account_info.try_borrow_data()?, |data| unsafe {
+            Self::from_bytes_unchecked(data)
+        }))
+    }
+
+    /// Return a `Clock` from the given account info.
+    ///
+    /// This method performs a check on the account info key, but does not
+    /// perform the borrow check.
+    ///
+    /// # Safety
+    ///
+    /// The caller must ensure that it is safe to borrow the account data – e.g., there are
+    /// no mutable borrows of the account data.
+    #[inline]
+    pub unsafe fn from_account_info_unchecked(
+        account_info: &AccountInfo,
+    ) -> Result<&Self, ProgramError> {
+        if account_info.key() != &CLOCK_ID {
+            return Err(ProgramError::InvalidArgument);
+        }
+        Ok(Self::from_bytes_unchecked(
+            account_info.borrow_data_unchecked(),
+        ))
+    }
+
+    /// Return a `Clock` from the given bytes.
+    ///
+    /// This method performs a length validation. The caller must ensure that `bytes` contains
+    /// a valid representation of `Clock`.
+    #[inline]
+    pub fn from_bytes(bytes: &[u8]) -> Result<&Self, ProgramError> {
+        if bytes.len() < Self::LEN {
+            return Err(ProgramError::InvalidArgument);
+        }
+        // SAFETY: `bytes` has been validated to be at least `Self::LEN` bytes long; the
+        // caller must ensure that `bytes` contains a valid representation of `Clock`.
+        Ok(unsafe { Self::from_bytes_unchecked(bytes) })
+    }
+
+    /// Return a `Clock` from the given bytes.
+    ///
+    /// # Safety
+    ///
+    /// The caller must ensure that `bytes` contains a valid representation of `Clock` and
+    /// that is has the expected length.
+    #[inline]
+    pub unsafe fn from_bytes_unchecked(bytes: &[u8]) -> &Self {
+        &*(bytes.as_ptr() as *const Clock)
+    }
 }
